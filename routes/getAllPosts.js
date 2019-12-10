3787c6083da9277de231c8169998397232471c5f
cloudberries27 <ccrod2298@gmail.com>
2019-12-10 14:10:14 -0500

    spelling

--- routes/getAllPosts.js
+++ routes/getAllPosts.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const firebase = require("firebase");
+const firebase = require('firebase');
 
 
 const firebaseConfig = {
