bf6f3953a12e1ff84c3dfc10da057120c5b2340f
cloudberries27 <ccrod2298@gmail.com>
2019-12-10 16:12:26 -0500

    connected to client side

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const firebase = require('firebase');
 
+
 const firebaseConfig = {
     apiKey: "AIzaSyBHS-03PLjmfmFU8pvHeRcCyqCR95_anIE",
     authDomain: "dwa-final.firebaseapp.com",
@@ -11,24 +12,27 @@ const firebaseConfig = {
     messagingSenderId: "511680695515",
     appId: "1:511680695515:web:439151a4e828374d38d0fe"
 };
-if (!firebase.apps.length) { 
-    firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
 const db = firebase.firestore();
 
-
-router.get("/", (req,res) => {
-    let TitleVal = req.query.title ? req.query.title: '';
-    let TextVal = req.query.text ? req.query.text: ''; 
-
-    db.collection("posts")
-    .add({       
-        title: TitleVal,
-        text: TextVal
+let postArr = [];
+db.collection('posts').get()
+    .then(post => {            
+            post.forEach(postData =>{
+                postArr.push(postData.data());
+            });            
+            console.log('Posts', postArr);
+        }
+    )
+    .catch(err => {
+        console.log('error', err);
     })
-    .then(ref => res.send(ref))
-    .catch(e => res.send(e));
-} )
+
+router.get('/', (req, res) => {
+    res.send(postArr);
+});
 
 module.exports = router;
