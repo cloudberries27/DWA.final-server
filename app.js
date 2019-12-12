241ca110dd87334b924272ac4da0d80c9dc93fdb
cloudberries27 <ccrod2298@gmail.com>
2019-12-11 22:41:43 -0500

    allowing from everywhere

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 const path = require("path"); //
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://fanamera.netlify.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
