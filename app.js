4bb0a6370509841fe4ce7c44bf1dd0c4eadb4422
cloudberries27 <ccrod2298@gmail.com>
2019-12-11 23:01:45 -0500

    missing line

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/firestore");
 
@@ -8,11 +7,14 @@ const app = express();
 const port = process.env.PORT || 4000;
 const path = require("path"); //
 
+app.use(express.json())
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 const index = require('./routes/index.js');
 const makePost = require('./routes/makePost.js');
 const singlePost = require('./routes/singlePost.js');
