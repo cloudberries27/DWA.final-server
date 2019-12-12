5f16828c115fbd5f634bb07f1a473fc9b02fed1f
cloudberries27 <ccrod2298@gmail.com>
2019-12-11 23:02:02 -0500

    missing line

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 const path = require("path"); //
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
